a36976acbdcb04758f1d096e8c6ce2e86965959b
shakil-ahmmed-se <shakilahmmed8906@gmail.com>
2024-10-10 08:22:54 +0600

    patch and delete method

--- src/routes/api/comments/[commentId]/+server.js
+++ src/routes/api/comments/[commentId]/+server.js
@@ -11,6 +11,20 @@ export function GET(requestEvent){
 export async function PATCH(requestEvent){
     const {params, request }  = requestEvent;
     const {commentId} = params;
+    const { text} = await request.json();
     const comment = comments.find(comment => comment.id === parseInt(commentId));
+    comment.text = text;
+    return json(comment);
+
+}
+
+export async function DELETE(requestEvent){
+    const {params} = requestEvent;
+    const {commentId} = params;
+    const deletedComment = comments.find(comment => comment.id === parseInt(commentId));
+    const index = comments.findIndex(comment => comment.id === parseInt(commentId));
+    comments.splice(index, 1);
+    return json(deletedComment);
+
 
 }
